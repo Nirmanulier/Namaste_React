05253ceff195d3b96be6c4ae922f0ed9c87127d6
Nirmanulier <nirman11940@gmail.com>
2026-02-25 15:54:40 +0530

    Updated React app and fixed errors

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";  
+
+
 const parent = React.createElement("div" , { id : "parent" } , [
-    React.createElement("div" , { id : "child"} , [
+    React.createElement("div" , { id : "child1"} , [
           React.createElement("h1" , {} , "I am h1 tag"),
           React.createElement("h2" , {} , "I am h2 tag")
     ]),
@@ -8,7 +12,7 @@ const parent = React.createElement("div" , { id : "parent" } , [
           React.createElement("h1" , {} , "I am h1 tag"),
           React.createElement("h2" , {} , "I am h2 tag")
     ])
-])
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
